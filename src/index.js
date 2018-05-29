ec6cddfa72efe38e488810590675980fcc22d17c
Colin <owens106@purde.edu>
2018-05-29 14:55:16 -0400

    add react router and wrap app in router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import {BrowserRouter as Router} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+
+<Router>
+    <App />, document.getElementById('root')
+</Router>
+
+
+)
+
+
+registerServiceWorker()
