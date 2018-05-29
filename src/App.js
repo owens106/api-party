7291abe9312fcd2a04b44789025ab9d0d2d3aa5e
Colin <owens106@purde.edu>
2018-05-29 15:06:20 -0400

    add style for github links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import {Route, Switch, NavLink} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="api-party">
-        <header>
-          <h3 className ='App'>no party like an API party </h3>
-          <h1 className='App'>api party</h1>
-        </header>
-       
+      <div className="App">
+        <div className ='App-header'>
+          <h3 >no party like an API party </h3>
+          <h1 >api party</h1>
+        </div>
+          <ul className ='navLinks'>
+            <li> 
+              <NavLink to ='/github'> Github API </NavLink>
+            </li>
+          </ul>
+
+
+       <Switch>
+        <Route path ='/github' render={() => <h1>Github!</h1>}/>
+        <Route render ={() => <p> to get started click a link above </p>}/>
+
+
+        
+      </Switch>
       </div>
     );
   }
